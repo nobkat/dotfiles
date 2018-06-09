30b3a278c1321de09e804733bb0156ba5497b744
Noboru Katayama <katayama@rs.tus.ac.jp>
2018-06-09 11:42:20 +0900

    modfiy installer

--- install.sh
+++ install.sh
@@ -3,10 +3,10 @@
 if [ -e ~/.zshrc ]; then
     mv ~/.zshrc ~/.zshrc_old
 fi
-ln -sf ~/dotfiles/zshrc ~/.zshrc
+ln -sf ./zshrc ~/.zshrc
 
 if [ -e ~/.vimrc ]; then
     mv ~/.vimrc ~/.vimrc_old
 fi
-ln -sf ~/dotfiles/vimrc ~/.vimrc
+ln -sf ./vimrc ~/.vimrc
 
